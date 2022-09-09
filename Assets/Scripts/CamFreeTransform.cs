2033d10179fed1eb95a6412f3ed397e76fa82965
ZhuLei <zhulei@xmfunny.com>
2022-09-09 09:56:36 +0800

    <feature> add cam script

---
+++ Assets/Scripts/CamFreeTransform.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CamFreeTransform : MonoBehaviour {
+    public float speed = 25f;
+    public float rotationSpeed = 2000f;
+
+    private Vector2 mousePos;
+    private bool _isFirstMove;
+
+    private Vector2 mousePosMove;
+    private bool _isFirstMoveMove;
+
+    private void OnGUI() {
+        GUILayout.Label("Go");
+    }
+    private void Start() {
+        mousePos = new Vector2(Screen.width - 400f, 400f);
+        _isFirstMove = true;
+    }
+
+    void Update() {
+        /*GameObject controlSphereMoveGUI = GameObject.Find("controlSphereMove");
+        GameObject controlSphereRotateGUI = GameObject.Find("controlSphereRotate");*/
+        if (Input.touchCount == 0) {
+            mousePos = new Vector2(Screen.width - 400f, 400f);
+            _isFirstMove = true;
+
+            mousePosMove = new Vector2(400f, 400f);
+            _isFirstMoveMove = true;
+        }
+        if (Input.touchCount == 1) {
+            if (Input.GetTouch(0).position.x > Screen.width / 2) {
+                _isFirstMoveMove = true;
+            } else {
+                _isFirstMove = true;
+            }
+        }
+        if (Input.touchCount > 0) {
+            /*            bool moveButton = false;
+                        bool rotateButton = false;*/
+            /*controlSphereMoveGUI.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(400f, 400f, Camera.main.nearClipPlane + 0.03f));
+            controlSphereRotateGUI.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 400f, 400f, Camera.main.nearClipPlane + 0.03f));*/
+            for (int i = 0; i < 2; i++) {
+                if (Input.GetTouch(i).position.x < Screen.width / 2) {
+                    //moveButton = true;
+                    if (_isFirstMoveMove == true) {
+                        mousePosMove = Input.GetTouch(i).position;
+                        _isFirstMoveMove = false;
+                    }
+                    if (_isFirstMoveMove == false) {
+                        float xDistance = Input.GetTouch(i).position.x - mousePosMove.x;
+                        transform.Translate(xDistance / 500f * Time.deltaTime * speed, 0, 0);
+                        float zDistance = Input.GetTouch(i).position.y - mousePosMove.y;
+                        transform.Translate(0, 0, zDistance / 500f * Time.deltaTime * speed);
+                    }
+                }
+                if (Input.GetTouch(i).position.x > Screen.width / 2) {
+                    //rotateButton = true;
+                    if (_isFirstMove == true) {
+                        mousePos = Input.GetTouch(i).position;
+                        _isFirstMove = false;
+                    }
+                    if (_isFirstMove == false) {
+                        float xDistance = Input.GetTouch(i).position.x - mousePos.x;
+                        transform.Rotate(0, xDistance / 200f * Time.deltaTime * rotationSpeed, 0, Space.World);
+                        float yDistance = -Input.GetTouch(i).position.y + mousePos.y;
+                        transform.Rotate(yDistance / 200f * Time.deltaTime * rotationSpeed, 0, 0, Space.Self);
+
+                        mousePos = Input.GetTouch(i).position;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(new Vector3(400f, 400f, Camera.main.nearClipPlane + 0.03f)), 0.03f);
+        Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 400f, 400f, Camera.main.nearClipPlane + 0.03f)), 0.03f);
+    }
+}
