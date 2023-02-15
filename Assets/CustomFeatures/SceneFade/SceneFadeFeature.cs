48198f8ff3568ff119a671e018778ce431daeb54
ZhuLei <zhulei@xmfunny.com>
2023-02-15 16:59:00 +0800

    <feature> begin scene fade

---
+++ Assets/CustomFeatures/SceneFade/SceneFadeFeature.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SceneFadeFeature : ScriptableRendererFeature {
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Create() {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/CustomFeatures/SceneFade/SceneFadePass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+public class SceneFadePass : ScriptableRenderPass {
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+
+    }
+}
